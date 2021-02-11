eb8c61af780daef6a01cc623e31dd7157501b661
zhichaoSun <zhichao_sun@outlook.com>
2021-02-11 12:27:40 +0800

    add comments

--- MergeSortedArrays.js
+++ MergeSortedArrays.js
@@ -1,4 +1,56 @@
+/*
+ * Merge Without Extra Space
+ *
+ * Given two sorted arrays arr1[] and arr2[] of sizes N and M in non-decreasing order.
+ * Merge them in sorted order without using any extra space.
+ * Modify arr1 so that it contains the first N elements and modify arr2 so that it contains the last M elements.
+ *
+ * eg:
+ * Input:
+ *   N = 4, arr1[] = [1 3 5 7]
+ *   M = 5, arr2[] = [0 2 6 8 9]
+ * Output:
+ *   arr1[] = [0 1 2 3]
+ *   arr2[] = [5 6 7 8 9]
+ *
+ */
+
+
+/***
+ * Easy to come up with
+ *
+ * O(N) aux space
+ *
+ * @param arr1
+ * @param arr2
+ * @param l1
+ * @param l2
+ */
 function merge1(arr1, arr2, l1, l2) {
+    let arrTemp = [...arr1, ...arr2]
+    arrTemp.sort((a, b) => a - b)
+    arr1.forEach((el, index, arr1) => 
+        arr1[index] = arrTemp[index]
+    )
+    arr2.forEach((el, index, arr2) => 
+        arr2[index] = arrTemp[index + l1]
+    )
+    
+    // console.log("arr1: ", arr1)
+    // console.log("arr2: ", arr2)
+}
+
+
+/***
+ *
+ * O(1) aux space
+ *
+ * @param arr1
+ * @param arr2
+ * @param l1
+ * @param l2
+ */
+function merge2(arr1, arr2, l1, l2) {
     let i1 = l1 - 1
     let i2 = 0;
     while(i2 < l2 && i1 >= 0) {
@@ -12,33 +64,16 @@ function merge1(arr1, arr2, l1, l2) {
         else {
             i1--
         }
-        console.log("arr1: ", arr1)
-        console.log("arr2: ", arr2)
     }
 
-    arr1.sort((a,b)=>a-b)
-    arr2.sort((a,b)=>a-b)
-    console.log("arr1: ", arr1)
-    console.log("arr2: ", arr2)
+    arr1.sort((a,b) => a - b)
+    arr2.sort((a,b) => a - b)
+
+    // console.log("arr1: ", arr1)
+    // console.log("arr2: ", arr2)
 }
 
-function merge2(arr1, arr2, l1, l2) {
-    let arrTemp = [...arr1, ...arr2]
-    arrTemp.sort((a,b)=>a-b)
-    arr1.forEach((el, index, arr1) => 
-        arr1[index] = arrTemp[index]
-    )
-    arr2.forEach((el, index, arr2) => 
-        arr2[index] = arrTemp[index + l1]
-    )
-    
-    console.log("arr1: ", arr1)
-    console.log("arr2: ", arr2)
-    console.log("arrT: ", arrTemp)
-    console.log("-------------")
-}
-
-merge1(
+merge2(
     [1, 36, 39, 105, 146, 154, 168, 170, 204, 206, 217, 219, 225, 227, 272, 282, 293, 300, 312, 323, 328, 328, 334, 335, 359, 370, 383, 392, 395, 396, 403, 413, 422, 437, 443, 448, 462, 463, 465, 479, 492, 496], 
     [7, 22, 30, 36, 38, 38, 39, 41, 42, 48, 49, 83, 85, 102, 107, 116, 119, 124, 127, 130, 140, 142, 145, 149, 159, 163, 165, 174, 174, 191, 205, 212, 224, 230, 242, 246, 254, 257, 258, 265, 279, 289, 306, 307, 309, 317, 324, 334, 341, 343, 351, 360, 369, 371, 377, 387, 391, 394, 430, 431, 432, 440, 443, 445, 447, 455, 467, 478],
     42, 68
--- RearrangeArrayAlternately.js
+++ RearrangeArrayAlternately.js
@@ -1,23 +1,66 @@
-function rearrange(arr, n) {
-    let i = 0
+/*
+ * Rearrange array alternatively
+ *
+ * Given a sorted array of positive integers.
+ * Your task is to rearrange the array elements alternatively
+ * i.e first element should be max value,
+ *     second should be min value,
+ *     third should be second max,
+ *     fourth should be second min and so on.
+ *
+ * eg:
+ */
 
-    while(i <= n / 2 + 3) {
-        arr.splice(i, 0, arr[n-1])
-        arr.pop()
-        i+=2
-    }
-   
-    console.log(arr)
-}
 
-function rearrange2(arr, n) {
+/***
+ * Mathematical solution
+ *
+ * O(N) aux space
+ *
+ *
+ * @param arr
+ * @param n
+ */
+function rearrange1(arr, n) {
     let arr2 = [...arr]
-    for(let i=0; i<n/2; i++) {
-        arr[2*i] = arr2[n-1-i]
-        arr[2*i+1] = arr2[i]
+    for(let i = 0; i < n/2; i++) {
+        arr[2*i] = arr2[n-1 - i]
+        arr[2*i + 1] = arr2[i]
     }
     if(n%2) arr.pop()
-    console.log(arr)
+
+    // console.log(arr)
 }
 
-rearrange([1,2,3,4,5,6,7], 7)
+
+/***
+ * Refer to: https://youtu.be/ZRoVWxBngX0
+ * Bind [current] and [the replacement of current] in to a single number
+ * so that we can retrieve both of them in to final result later
+ *
+ * O(1) aux space
+ *
+ * @param arr
+ * @param n
+ */
+function rearrange2(arr, n) {
+    let max = n-1, min = 0, maxEl = arr[n-1] + 1; // maxEL need to be greater than arr[n-1] at least
+    for(let i = 0; i < n; i++) {
+        if(i%2) {
+            arr[i] += arr[min] % maxEl * maxEl
+            min++
+        } else {
+            arr[i] += arr[max] % maxEl * maxEl
+            max--
+        }
+    }
+
+    for(let i = 0; i < n; i++) {
+        arr[i] = parseInt(arr[i] / maxEl)
+    }
+
+    // console.log(arr)
+}
+
+rearrange2([1,2,3,4,5,6,7], 7)
+rearrange2([1,2,3,4,5,6,7,8], 8)
