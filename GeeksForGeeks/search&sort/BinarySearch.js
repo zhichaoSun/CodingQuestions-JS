362b0e9e064388092b50db5b5e680bffa9dd632f
zhichaoSun <zhichao_sun@outlook.com>
2021-02-16 11:52:38 +0800

    add Binary Search and comments

--- GeeksForGeeks/search&sort/BinarySearch.js
+++ GeeksForGeeks/search&sort/BinarySearch.js
@@ -1,18 +1,67 @@
-export function binarySearch(arr, left, right, el) {
+/***
+ *
+ * Binary Search
+ *
+ * @param arr
+ * @param n
+ * @param el
+ * @returns {number}: index or -1 when not foundd
+ */
+export function binarySearch(arr, n, el) {
+    let left = 0, right = arr.length-1
+
+    while(left <= right) {
+        let mid = Math.floor((left + right)/2)
+        console.log("left:", left, "mid:", mid, "right:", right)
+
+        if(arr[mid] === el) return mid
+        else if(arr[mid] > el) right = mid - 1
+        else left = mid + 1
+
+
+        console.log("left:", left, "mid:", mid, "right:", right)
+        console.log()
+    }
+    return -1
+}
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 124, 155, 1252]
+//   0  1  2  3  4  5  6  7  8  9   10  11   12   13
+const el = 1252
+const ans = binarySearch(arr, arr.length, el)
+console.log("ans:", ans)
+
+
+/***
+ *
+ * Binary Search (recursively)
+ *
+ * @param arr
+ * @param left
+ * @param right
+ * @param el
+ * @returns {number}: index or -1 when not found
+ *
+ */
+export function binarySearchR(arr, left, right, el) {
     if(left > right) return -1;
 
     let i = Math.floor((right+left)/2);
     console.log("i:", i)
 
-    if(arr[i] == el) return i;
-    else if(arr[i] < el) return binarySearch(arr, i+1, right, el);
-    else if(arr[i] > el) return binarySearch(arr, left, i-1, el);
+    if(arr[i] === el) return i;
+    else if(arr[i] < el) return binarySearchR(arr, i+1, right, el);
+    else if(arr[i] > el) return binarySearchR(arr, left, i-1, el);
 }
 
 // const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 124, 155, 1252]
-// const el = 124
-// const ans = binarySearch(arr, 0, arr.length, el)
+//         //   0  1  2  3  4  5  6  7  8  9   10  11   12   13
+// const el = 1252
+// const ans = binarySearchR(arr, 0, arr.length, el)
 // console.log("ans:", ans)
+
+
+
 
 
 export function binarySearchUpperB(arr, left, right, el) {
