ca0277bb60144aba6e6237f6a7d6c76ba773d74f
zhichaoSun <zhichao_sun@outlook.com>
2021-02-18 14:22:13 +0800

    add test cases for validate subsequence

---
+++ AE/array/test/ValidateSubsequence.test.js
@@ -0,0 +1,23 @@
+import {expect, test} from "@jest/globals";
+
+import {
+    isValidSubsequence
+} from "../ValidateSubsequence"
+
+test("Case 1", () => {
+    const array = [5, 1, 22, 25, 6, -1, 8, 10]
+    const sequence = [1, 6, -1, 10]
+    expect(isValidSubsequence(array, sequence)).toBe(true)
+})
+
+test("Case 2", () => {
+    const array = [5, 1, 22, 25, 6, -1, 8, 10]
+    const sequence = [5, 1, 22, 25, 6, -1, 8, 10, 10]
+    expect(isValidSubsequence(array, sequence)).toBe(false)
+})
+
+test("Case 3", () => {
+    const array = [5, 1, 22, 25, 6, -1, 8, 10]
+    const sequence = [5, 1, 22, 25, 6, -1, 8, 10]
+    expect(isValidSubsequence(array, sequence)).toBe(true)
+})
