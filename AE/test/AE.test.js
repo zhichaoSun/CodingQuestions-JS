6c3cbc3ebf56ad619f2ae4e8911b7de76172ed57
zhichaoSun <zhichao_sun@outlook.com>
2021-02-18 23:48:09 +0800

    using "describe" to group tests together

--- AE/test/AE.test.js
+++ AE/test/AE.test.js
@@ -1,27 +1,34 @@
-import {expect, test} from "@jest/globals";
+import {describe, expect, test} from "@jest/globals";
 
 import {
     isValidSubsequence,
     isValidSubsequence2
 } from "../array/ValidateSubsequence"
 
-test("validate subsequence case 1", () => {
-    const array = [5, 1, 22, 25, 6, -1, 8, 10]
-    const sequence = [1, 6, -1, 10]
-    expect(isValidSubsequence(array, sequence)).toBe(true)
-    expect(isValidSubsequence2(array, sequence)).toBe(true)
+describe("Validate subsequence: ", () => {
+
+    test("case 1", () => {
+        const array = [5, 1, 22, 25, 6, -1, 8, 10]
+        const sequence = [1, 6, -1, 10]
+        expect(isValidSubsequence(array, sequence)).toBe(true)
+        expect(isValidSubsequence2(array, sequence)).toBe(true)
+    })
+
+    test("case 2", () => {
+        const array = [5, 1, 22, 25, 6, -1, 8, 10]
+        const sequence = [5, 1, 22, 25, 6, -1, 8, 10, 10]
+        expect(isValidSubsequence(array, sequence)).toBe(false)
+        expect(isValidSubsequence2(array, sequence)).toBe(false)
+    })
+
+    test("case 3", () => {
+        const array = [5, 1, 22, 25, 6, -1, 8, 10]
+        const sequence = [5, 1, 22, 25, 6, -1, 8, 10]
+        expect(isValidSubsequence(array, sequence)).toBe(true)
+        expect(isValidSubsequence2(array, sequence)).toBe(true)
+    })
 })
 
-test("validate subsequence case 2", () => {
-    const array = [5, 1, 22, 25, 6, -1, 8, 10]
-    const sequence = [5, 1, 22, 25, 6, -1, 8, 10, 10]
-    expect(isValidSubsequence(array, sequence)).toBe(false)
-    expect(isValidSubsequence2(array, sequence)).toBe(false)
-})
 
-test("validate subsequence case 3", () => {
-    const array = [5, 1, 22, 25, 6, -1, 8, 10]
-    const sequence = [5, 1, 22, 25, 6, -1, 8, 10]
-    expect(isValidSubsequence(array, sequence)).toBe(true)
-    expect(isValidSubsequence2(array, sequence)).toBe(true)
-})
+
+
