7b3be0df9a7128cf508d514022926c477259da9d
zhichaoSun <zhichao_sun@outlook.com>
2021-02-19 00:07:06 +0800

    add comments

--- AE/recursion/NthFibonacci.js
+++ AE/recursion/NthFibonacci.js
@@ -1,23 +1,46 @@
 /*
- The Fibonacci sequence is defined as follows: 
- the first number of the sequence is 0, the second number is 1,
- and the nth number is the sum of the (n - 1)th and (n - 2)th numbers. 
- 
- Write a function that takes in an integer n and returns the nth Fibonacci number.
-
+ *
+ * The Fibonacci sequence is defined as follows:
+ *   the first number of the sequence is 0,
+ *   the second number is 1,
+ *   and the nth number is the sum of the (n - 1)th and (n - 2)th numbers.
+ *
+ * Write a function that takes in an integer n and returns the nth Fibonacci number
+ *
  */
 
+/***
+ *
+ * Recursive solution
+ *
+ * O(2^n) time complexity
+ * O(n) aux space
+ *
+ * @param n
+ * @returns {number|*}
+ *
+ */
 export function getNthFib(n) {
-	if(n == 0) return 0
-	if(n == 1) return 0
-	if(n == 2) return 1
-	
+	if(n === 0) return 0
+	if(n === 1) return 0
+	if(n === 2) return 1
 	return getNthFib(n-1) + getNthFib(n-2)
 }
 
+
+/***
+ *
+ * Normal solution
+ *
+ * O(n) time complexity
+ * O(1) aux space
+ *
+ * @param n
+ * @returns {number|*}
+ *
+ */
 export function getNthFib2(n) {
-	let lastTwoNum = [0, 1]
-    let count = 3
+	let lastTwoNum = [0, 1], count = 3
     while(count <= n) {
         let nextNum = lastTwoNum[0] + lastTwoNum[1]
         lastTwoNum[0] = lastTwoNum[1]
