e12739c22c24e8dfbfad1992f2c19085b7332c1e
zhichaoSun <zhichao_sun@outlook.com>
2021-04-08 19:37:36 +0800

    save and load

---
+++ AE/graph/RiverSizes.js
@@ -0,0 +1,23 @@
+
+// [
+//     [1, 0, 0, 1, 0],
+//     [1, 0, 1, 0, 0],
+//     [0, 0, 1, 0, 1],
+//     [1, 0, 1, 0, 1],
+//     [1, 0, 1, 1, 0]
+// ]
+
+function riverSizes(matrix) {
+    const reivers = []
+    const adj = new Set()
+
+    let x = 0, y = 0
+    const h = matrix[0].length, w = matrix[0][0].length
+
+    while(x < h && y < w) {
+        if(matrix[x][y] === 1) {
+
+        }
+    }
+
+}
